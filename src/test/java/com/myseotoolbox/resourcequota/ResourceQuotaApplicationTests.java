e94a6b5c0379a2e5cd0d94dfd7bf1643d8d3bfd5
Salvatore Novelli <salvatore.novelli@gmail.com>
2022-06-05 21:18:12 +0100

    init

---
+++ src/test/java/com/myseotoolbox/resourcequota/ResourceQuotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myseotoolbox.resourcequota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceQuotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
