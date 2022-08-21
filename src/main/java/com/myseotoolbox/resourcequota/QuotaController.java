3f565c2ed215db11216e0075082299dd7befee9f
Salvatore Novelli <salvatore.novelli@gmail.com>
2022-08-21 14:32:33 +0100

    adding testing on controller side and refining exceptions handling

---
+++ src/main/java/com/myseotoolbox/resourcequota/QuotaController.java
@@ -0,0 +1,30 @@
+package com.myseotoolbox.resourcequota;
+
+import io.github.quota4j.ResourceQuotaNotFoundException;
+import io.github.quota4j.UserQuotaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class QuotaController {
+
+    private final UserQuotaService userQuotaService;
+
+    @GetMapping("/resources/{resourceId}/workspaces/{workspaceId}")
+    public ResponseEntity<String> acquireResource(@PathVariable String resourceId, @PathVariable String workspaceId, @RequestParam(defaultValue = "1") int quantity) {
+        try {
+            if (!userQuotaService.tryAcquire(workspaceId, resourceId, quantity))
+                return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        } catch (ResourceQuotaNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
--- src/test/java/com/myseotoolbox/resourcequota/QuotaControllerTest.java
+++ src/test/java/com/myseotoolbox/resourcequota/QuotaControllerTest.java
@@ -1,7 +1,88 @@
 package com.myseotoolbox.resourcequota;
 
+
+import io.github.quota4j.ResourceQuotaNotFoundException;
+import io.github.quota4j.UserQuotaService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
 class QuotaControllerTest extends DataRedisContainerTest {
 
+    private static final String NON_EXISTING_RESOURCE_ID = "crawler.non_existing_resource";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserQuotaService userQuotaService;
 
 
+    @Test
+    void shouldReturnOkIfQuotaIsAvailable() throws Exception {
+        givenResourceQuota("crawler.crawls_per_day")
+                .withRemainingTokens(10)
+                .build();
+
+        mockMvc.perform(get("/resources/crawler.crawls_per_day/workspaces/123456"))
+                .andExpect(status().is(OK.value()));
+    }
+
+    @Test
+    void shouldReturn() throws Exception {
+        givenResourceQuota("crawler.crawls_per_day")
+                .withRemainingTokens(0)
+                .build();
+
+        mockMvc.perform(get("/resources/crawler.crawls_per_day/workspaces/123456"))
+                .andExpect(status().is(TOO_MANY_REQUESTS.value()));
+    }
+
+    @Test
+    void shouldHandleResourceNotFound() throws Exception {
+        Mockito.when(userQuotaService.tryAcquire(anyString(), anyString(), anyInt())).thenThrow(new ResourceQuotaNotFoundException(NON_EXISTING_RESOURCE_ID));
+
+        mockMvc.perform(get("/resources/" + NON_EXISTING_RESOURCE_ID + "/workspaces/123456"))
+                .andExpect(status().is(NOT_FOUND.value()))
+                .andExpect(content().string(containsString(NON_EXISTING_RESOURCE_ID)));
+    }
+
+    private QuotaBuilder givenResourceQuota(String resourceId) {
+        return new QuotaBuilder(resourceId);
+    }
+
+    private class QuotaBuilder {
+        private final String resourceId;
+        private int remainingTokens;
+
+        public QuotaBuilder(String resourceId) {
+            this.resourceId = resourceId;
+        }
+
+        public QuotaBuilder withRemainingTokens(int remainingTokens) {
+            this.remainingTokens = remainingTokens;
+            return this;
+        }
+
+        public void build() {
+            Mockito.when(userQuotaService.tryAcquire(anyString(), Mockito.eq(resourceId), anyInt())).thenAnswer(invocation -> {
+                Integer quantity = invocation.getArgument(2, Integer.class);
+                remainingTokens -= quantity;
+                return remainingTokens > 0;
+            });
+        }
+    }
 }
